a8fd789036a0d7ad47731f6630e6aaf0e86d4869
Matheus <matheussdem65@gmail.com>
2025-04-14 18:11:36 -0300

    Model Login

--- PW3/Controllers/LoginController.cs
+++ PW3/Controllers/LoginController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PW3.Models;
 
 namespace PW3.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Logar()
+        [HttpGet]
+        public IActionResult Logar(LoginModel dados)
         {
-            return View();
+            LoginModel model = new LoginModel();
+            model.Login = "matheussdem65@gmail.com";
+            model.Senha = "433221";
+            return View(model); //Lembra de colocar model para fazer conecao com o LoginModel
         }
     }
 }
