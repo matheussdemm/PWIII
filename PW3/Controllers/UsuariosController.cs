e470e44474f23a55fbd6b48db4e7ac2294305731
Matheus <matheussdem65@gmail.com>
2025-06-02 16:50:52 -0300

    Adicionei CRUD no meu site.

--- PW3/Controllers/UsuariosController.cs
+++ PW3/Controllers/UsuariosController.cs
@@ -29,6 +29,34 @@ namespace PW3.Controllers
             connection.Close();
             return View(model);
         }
+        public IActionResult Create() 
+        {
+            return View();
+             }
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+
+        public IActionResult Lista() {
+            List<UsuarioEntidade> model = new List<UsuarioEntidade>();
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            var cmd = new MySqlCommand("SELECT id, email FROM usuarios", connection);
+            using var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                UsuarioEntidade u = new UsuarioEntidade();
+                u.Id = reader.GetInt32("id");
+                u.Email = reader.GetString("email");
+                model.Add(u);
+            }
+            connection.Close();
+            return View(model);
+        }
     }
 
 }
